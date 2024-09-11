c97bf78232e18b7bbfde0e7dc1e59e8fcb1b7c79
Leonid <work.leonid@gmail.com>
2024-09-11 11:39:13 +0300

    fix

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,4 @@ export { default as NavBtns } from "./NavBtns.svelte";
 export { default as Header } from "./Header.svelte";
 export { default as Footer } from "./Footer.svelte";
 export { default as HeadlineCard } from "./HeadlineCard.svelte";
+export { default as ImageDescription } from "./ImageDescription.svelte";
