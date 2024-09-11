37c2c5e445b6e1017c921020e55669fc355e2e68
Leonid <work.leonid@gmail.com>
2024-09-11 11:06:36 +0300

    fix

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // Export components for use in multiple routes
-export { default as Card } from "./Card.svelte";
-export { default as StepperCard } from "./StepperCard.svelte";
-export { default as NavBtns } from "./NavBtns.svelte";
+export { default as StepperCard } from "./steppercard.svelte";
+export { default as NavBtns } from "./navbtns.svelte";
