3d44c597472e4922d168a6296888ed8010d2266c
Leonid <work.leonid@gmail.com>
2024-09-11 11:27:03 +0300

    fix

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-// Export components for use in multiple routes
+export { default as NavBtns } from "./NavBtns.svelte";
+export { default as Header } from "./Header.svelte";
+export { default as Footer } from "./Footer.svelte";
+export { default as HeadlineCard } from "./HeadlineCard.svelte";
